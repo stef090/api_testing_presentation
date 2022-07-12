1d56aaf56ff9bc9940e7cef6ea35cdcf95e001df
slovasic <stjepan.lovasic@intis.hr>
2022-07-12 22:03:54 +0200

    main: Preparation complete

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,11 +9,17 @@ client = TestClient(app)
 API_COUNTRY_TEST_DATA = [("HR", "10000", 200, "Croatia")]
 
 
-# TODO: PARAMETRIZE
-@pytest.mark.parametrize("country_code, postal_code, expected_status, expected_country", API_COUNTRY_TEST_DATA)
+@pytest.mark.parametrize(
+    "country_code, postal_code, expected_status, expected_country",
+    API_COUNTRY_TEST_DATA,
+)
 def test_api_country_code(country_code, postal_code, expected_status, expected_country):
     url = f"{API_PREFIX}/{country_code}/{postal_code}"
     response = client.get(url=url)
     assert response.status_code == expected_status
     response_body = response.json()
     assert response_body["country"] == expected_country
+
+
+def test_api_country_code_mocked(mocker):
+    mocker.patch("requests.get", return_value=...)
